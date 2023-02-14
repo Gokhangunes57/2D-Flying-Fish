df53cf2224a7dd68bdfd9032e59de7fbe3ae823e
Gökhan güneş <132ggokhann@gmail.com>
2023-02-14 19:08:55 +0300

    Fish rotation added
    version 1.7

--- Flying Fish/Assets/Scripts/Fish.cs
+++ Flying Fish/Assets/Scripts/Fish.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class Fish : MonoBehaviour
 {
    Rigidbody2D rb;
+   [SerializeField] float speed = 9f;
+   private int angle;
+   private int MaxAngle = 20;
+   private int MinAngle = -60;
+   
     void Start()
     {
       rb=GetComponent<Rigidbody2D>();  
@@ -14,12 +19,40 @@ public class Fish : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+     FishSwim();
+     FishRotation();
+
+        
+        
+            
+       
+    }
+
+    void FishSwim()
+    {
         if (Input.GetMouseButtonDown(0))
         {
-            rb.velocity = new Vector2(rb.velocity.x, 9f);
+            rb.velocity = Vector2.zero;
+            rb.velocity = new Vector2(rb.velocity.x, speed);
         }
+    }
+
+    void FishRotation()
+    {
+        if (rb.velocity.y > 0)
         {
-            
+            if(angle<=MaxAngle)
+            {
+                angle += 4;
+            }
         }
+        else if(rb.velocity.y<0)
+        {
+            if(angle>=MinAngle)
+            {
+                angle -= 2;
+            }
+        }
+        transform.rotation = Quaternion.Euler(0, 0, angle);
     }
 }
