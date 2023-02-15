7618730a5eb5605f7fd3a237987020bd2584ddaa
Gökhan güneş <132ggokhann@gmail.com>
2023-02-15 15:13:21 +0300

    ground loop completed
    version 1.9

---
+++ Flying Fish/Assets/Scripts/LeftMovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftMovement : MonoBehaviour
+{
+    public float speed;
+    private BoxCollider2D box;
+    private float width;
+    void Start()
+    {
+        box = GetComponent<BoxCollider2D>();
+        width = box.size.x;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        if (transform.position.x <= -width)
+        {
+            transform.position = new Vector2(transform.position.x + 2*width, transform.position.y);
+        }
+    }
+}
