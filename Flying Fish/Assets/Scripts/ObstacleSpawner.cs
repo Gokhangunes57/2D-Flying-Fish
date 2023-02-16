fdc9860c9f1680cbf7d4767031663bfa8f9e7783
Gökhan güneş <132ggokhann@gmail.com>
2023-02-16 21:47:16 +0300

    Random y value added
    version 2.3

--- Flying Fish/Assets/Scripts/ObstacleSpawner.cs
+++ Flying Fish/Assets/Scripts/ObstacleSpawner.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject obstacle;
+    public float MaxTime ;
+    float timer;
+    public float MinY;
+    public float MaxY;
+    float randomY;
+    
+    
+    
     void Start()
     {
-        
+       SpawnObstacle();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >=MaxTime)
+        { 
+            
+            randomY = Random.Range(MinY, MaxY);
+            SpawnObstacle();
+            timer = 0;
+        }
+      
+    }
+    
+    public void SpawnObstacle()
+    {
+      GameObject newObstacle = Instantiate(obstacle);
+      newObstacle.transform.position = new Vector2(transform.position.x,randomY);
+      
     }
 }
